4e2e915530593326ddcf085e3be36032ae3c7abc
Camdaman21 <cameronmm21@icloud.com>
2023-12-08 08:51:01 -0500

    decent amount of work on tooltip

---
+++ UnityRTS/Assets/Scripts/TooltipScreenspaceUI.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TooltipScreenspaceUI : MonoBehaviour
+{
+
+    [SerializeField] private RectTransform canvasRectTransform;
+
+    private RectTransform backgroundRectTransform;
+    private TextMeshProUGUI textMeshPro;
+    private RectTransform rectTransform;
+
+  private void Awake()
+    {
+        backgroundRectTransform = transform.Find("background").GetComponent<RectTransform>();
+        textMeshPro = transform.Find("text").GetComponent<TextMeshProUGUI>();
+        rectTransform = transform.GetComponent<RectTransform>();
+
+        SetText("Hello World");
+    }
+    private void SetText(string tooltipText)
+    {
+        textMeshPro.SetText(tooltipText);
+        textMeshPro.ForceMeshUpdate();
+
+        Vector2 textSize = textMeshPro.GetRenderedValues(false);
+        Vector2 paddingSize = new Vector2(8, 8);
+
+        backgroundRectTransform.sizeDelta = textSize + paddingSize;
+    }
+
+    private void Update()
+    {
+        rectTransform.anchoredPosition = Input.mousePosition;
+    }
+}
