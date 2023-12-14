ed73d677cbef065b710bdca0ce0eb6426e5b7913
Camdaman21 <cameronmm21@icloud.com>
2023-12-14 10:27:40 -0500

    tooltip work

---
+++ UnityRTS/Assets/Scripts/TooltipTest.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TooltipTest : MonoBehaviour
+{
+    private float timer;
+
+    private void Start()
+    {
+        System.Func<string> gettooltipTextfunc = () =>
+        {
+            return "This is my tooltip!\nThere are many like it but this one is mine!\n" + timer;
+        };
+        TooltipCanvas.ShowTooltip_Static("This is my tooltip!\nThere are many like it but this one is mine!");
+    }
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+    }
+}
